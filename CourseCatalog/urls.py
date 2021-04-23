9e276990a7670aac28ca7d8563c7f9a49f081faa
Sergey Derevianko <derevankosergej0@gmail.com>
2021-04-23 20:00:36 +0300

    Change setting

--- CourseCatalog/urls.py
+++ CourseCatalog/urls.py
@@ -2,9 +2,11 @@ from courses.views import index
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('courses.urls')),
-    path('', index),
-]
+    path('', index)
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,9 @@
 from .views import ListCoursesAPIView, DetailCourseAPIView
 
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('courses/', ListCoursesAPIView.as_view(), name='courses_list'),
     path('courses/<int:pk>', DetailCourseAPIView.as_view(), name='course_by_id'),
     path('courses/<str:title>', ListCoursesAPIView.as_view(), name='course_by_title'),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
